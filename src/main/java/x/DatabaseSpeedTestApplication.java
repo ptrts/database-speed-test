aa170a17a62313f91acb7dfce61a0e6d3b8972ed
Pavel Taruts <ptrts@mail.ru>
2025-12-29 14:11:20 +0300

    -

---
+++ src/main/java/x/DatabaseSpeedTestApplication.java
@@ -0,0 +1,13 @@
+package x;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DatabaseSpeedTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DatabaseSpeedTestApplication.class, args);
+    }
+
+}
---
+++ src/test/java/x/DatabaseSpeedTestApplicationTests.java
@@ -0,0 +1,13 @@
+package x;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DatabaseSpeedTestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
