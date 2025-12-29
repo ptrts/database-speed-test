9f5b7e3c6b20ac10019734a7cdabb781d4a0c007
Pavel Taruts <ptrts@mail.ru>
2025-12-29 14:18:10 +0300

    -

---
+++ sql/00-create-database.sql
@@ -0,0 +1,2 @@
+create database database_speed_test
+    with owner postgres;
--- src/main/java/x/App.java
+++ src/main/java/x/App.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class DatabaseSpeedTestApplication {
+public class App {
 
     public static void main(String[] args) {
-        SpringApplication.run(DatabaseSpeedTestApplication.class, args);
+        SpringApplication.run(App.class, args);
     }
 
 }
