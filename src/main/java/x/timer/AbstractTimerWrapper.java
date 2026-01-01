2f63bb65f51a497c207e0cad94ec18da5b00ffe1
Pavel Taruts <ptrts@mail.ru>
2026-01-02 02:25:04 +0300

    Добавляем клиентский процентиль 0.99

--- src/main/java/x/timer/AbstractTimerWrapper.java
+++ src/main/java/x/timer/AbstractTimerWrapper.java
@@ -17,7 +17,7 @@ public abstract class AbstractTimerWrapper {
         this.timerProvider = Timer
                 .builder(name)
                 .description(description)
-                .publishPercentiles(0.95)
+                .publishPercentiles(0.95, 0.99)
                 .withRegistry(registry)
         ;
     }
